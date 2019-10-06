46163c73dcffc6fa02ee2aa2dedb8adeecd858fe
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-06 15:06:23 +0700

    Create Diagonal Difference.js

---
+++ Easy/Diagonal Difference.js
@@ -0,0 +1,69 @@
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+
+Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+Input Format
+
+The first line contains a single integer, , the number of rows and columns in the matrix . 
+Each of the next  lines describes a row, , and consists of  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19 
+Difference: |4 - 19| = 15
+
+Note: |x| is the absolute value of x
+*/
+function diagonalDifference(arr) {
+    let left = 0
+    let right = 0
+    let j=0,k=arr.length-1
+    for (let i=0;i<arr.length;){
+            left+=arr[i][j]
+            right+=arr[i][k]
+            i++
+            j++
+            k--
+    }
+    return Math.abs(left-right)
+}
