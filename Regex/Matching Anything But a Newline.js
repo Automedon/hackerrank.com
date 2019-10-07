977be023ab523988cac3bb6bcceb89df90394289
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-07 16:03:32 +0700

    Create Matching Anything But a Newline.js

---
+++ Regex/Matching Anything But a Newline.js
@@ -0,0 +1,22 @@
+/*
+dot
+
+The dot (.) matches anything (except for a newline).
+
+ach02.png
+
+
+Note: If you want to match (.) in the test string, you need to escape the dot by using a slash \..
+In Java, use \\. instead of \..
+
+Task
+
+You have a test string .
+Your task is to write a regular expression that matches only and exactly strings of form: , where each variable  can be any single character except the newline.
+
+Note
+
+This is a regex only challenge. You are not required to write any code.
+You only have to fill in the regex pattern in the blank (_________).
+*/
+var Regex_Pattern = /^...\....\....\....$/g; 
