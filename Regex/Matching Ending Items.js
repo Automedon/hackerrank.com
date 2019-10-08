acff47b14b09b564ad8651db6cfc7f1e014aa5d5
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-08 10:35:03 +0700

    Create Matching Ending Items.js

---
+++ Regex/Matching Ending Items.js
@@ -0,0 +1,16 @@
+/*
+The $ boundary matcher matches an occurrence of a character/character class/group at the end of a line.
+
+matching end items.png
+
+
+Task
+
+Write a RegEx to match a test string, , under the following conditions:
+
+ should consist of only lowercase and uppercase letters (no numbers or symbols).
+ should end in s.
+This is a RegEx-only challenge, so you are not required to write any code.
+Simply replace the blank (_________) with your RegEx pattern.
+*/
+var Regex_Pattern = /^[a-zA-Z]*s$/;
