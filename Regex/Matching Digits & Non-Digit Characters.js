37e70ecba28157512eac8b1da89497e617426ce9
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-08 08:48:37 +0700

    Create Matching Digits & Non-Digit Characters.js

---
+++ Regex/Matching Digits & Non-Digit Characters.js
@@ -0,0 +1,23 @@
+/*
+The expression \d matches any digit [ - ].
+
+ach04_01.png
+
+
+
+The expression \D matches any character that is not a digit.
+
+ach04_02.png
+
+
+Task
+
+You have a test string . Your task is to match the pattern 
+Here  denotes a digit character, and  denotes a non-digit character.
+
+Note
+
+This is a regex only challenge. You are not required to write any code.
+You only have to fill the regex pattern in the blank (_________).
+*/
+var Regex_Pattern = /\d\d\D\d\d\D\d\d\d\d/;
