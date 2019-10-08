f01906f7c8b9c6d75d69190dfb48b31eb0091d36
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-08 08:51:35 +0700

    Create Matching Whitespace & Non-Whitespace Character.js

---
+++ Regex/Matching Whitespace & Non-Whitespace Character.js
@@ -0,0 +1,21 @@
+/*
+\s matches any whitespace character [ \r\n\t\f ].
+
+ach05_01.png
+
+
+\S matches any non-white space character.
+
+ach05_02.png
+
+Task
+
+You have a test string . Your task is to match the pattern 
+Here,  denotes whitespace characters, and  denotes non-white space characters.
+
+Note
+
+This is a regex only challenge. You are not required to write code.
+You have to fill the regex pattern in the blank (_________).
+*/
+var Regex_Pattern = /\S\S\s\S\S\s\S\S/;
