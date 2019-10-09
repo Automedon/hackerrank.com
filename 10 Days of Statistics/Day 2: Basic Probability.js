1ef9d49abe457194053c9d8d43d15076b040b052
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-09 11:00:12 +0700

    Create Day 2: Basic Probability.js

---
+++ 10 Days of Statistics/Day 2: Basic Probability.js
@@ -0,0 +1,9 @@
+/*
+Objective
+In this challenge, we practice calculating probability. Check out the Tutorial tab for a breakdown of probability fundamentals!
+
+
+Task
+In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most .
+*/
+<p>5 / 6</p>
