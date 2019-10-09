60a2ace0457c15733915768e2320271a3ef694ce
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-09 11:01:14 +0700

    Create Day 2: Compound Event Probability.js

---
+++ 10 Days of Statistics/Day 2: Compound Event Probability.js
@@ -0,0 +1,16 @@
+/*
+Objective
+In this challenge, we practice calculating the probability of a compound event. We recommend you review today's Probability Tutorial before attempting this challenge.
+
+
+Task
+There are  urns labeled , , and .
+
+
+Urn  contains  red balls and  black balls.
+Urn  contains  red balls and  black balls.
+Urn  contains  red balls and  black balls.
+
+One ball is drawn from each of the  urns. What is the probability that, of the  balls drawn,  are red and  is black?
+*/
+17 / 42
