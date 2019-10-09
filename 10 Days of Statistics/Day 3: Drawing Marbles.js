4e24cf8062326b1649d0f5be3d1d3ca535eee8d7
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-09 11:05:38 +0700

    Create Day 3: Drawing Marbles.js

---
+++ 10 Days of Statistics/Day 3: Drawing Marbles.js
@@ -0,0 +1,9 @@
+/*
+Objective
+In this challenge, we're reinforcing what we've learned today. In case you've missed them, today's tutorials are on Conditional Probability and Combinations and Permutations.
+
+
+Task
+A bag contains  red marbles and  blue marbles. Then,  marbles are drawn from the bag, at random, without replacement. If the first marble drawn is red, what is the probability that the second marble is blue?
+*/
+2 / 3
