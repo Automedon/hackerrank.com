c1e666623fceb425b8e5f33040b730da59f41847
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-09 11:03:54 +0700

    Create Day 3: Conditional Probability.js

---
+++ 10 Days of Statistics/Day 3: Conditional Probability.js
@@ -0,0 +1,9 @@
+/*
+Objective
+In this challenge, we get started with conditional probability. Check out the Tutorial tab for learning materials!
+
+
+Task
+Suppose a family has  children, one of which is a boy. What is the probability that both children are boys?
+*/
+1 / 3
