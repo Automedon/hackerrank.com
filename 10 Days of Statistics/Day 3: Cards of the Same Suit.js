df3d91bb2a9f125fd9eadc3e871bc2b7913531d8
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-09 11:04:54 +0700

    Create Day 3: Cards of the Same Suit.js

---
+++ 10 Days of Statistics/Day 3: Cards of the Same Suit.js
@@ -0,0 +1,9 @@
+/*
+Objective
+In this challenge, we're getting started with combinations and permutations. Check out the Tutorial tab for learning materials!
+
+
+Task
+You draw  cards from a standard -card deck without replacing them. What is the probability that both cards are of the same suit?
+*/
+12 / 51
