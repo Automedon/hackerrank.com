908936197d79facc2be365ec77d05f074b9b5988
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-09 11:00:43 +0700

    Create Day 2: More Dice.js

---
+++ 10 Days of Statistics/Day 2: More Dice.js
@@ -0,0 +1,9 @@
+/*
+Objective
+In this challenge, we practice calculating probability. We recommend you review the previous challenge's Tutorial before attempting this problem.
+
+
+Task
+In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of .
+*/
+1 / 9
