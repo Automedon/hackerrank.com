5dff3d42a4bcab5704f12c5a9d2cea010b25eb63
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-10 13:18:01 +0700

    Create Day 7: Arrays.js

---
+++ 30 Days of code/Day 7: Arrays.js
@@ -0,0 +1,33 @@
+/*
+Objective
+Today, we're learning about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Input Format
+
+The first line contains an integer,  (the size of our array).
+The second line contains  space-separated integers describing array 's elements.
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+*/
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    console.log(arr.reverse().join` `)
+}
